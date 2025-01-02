0f6b8d979b7bd2ee4896fe77eb5b528bcd40917e
alex_nout <nsk540010@gmail.com>
2025-01-02 14:08:19 +0700

    тесты с логгированием

--- bot.py
+++ bot.py
@@ -11,20 +11,22 @@ from aiogram import Bot, Dispatcher
 # Импорт ваших хендлеров
 from handlers import start, group_games, questions, different_types, usernames, animals, game_1_100, Seva
 
+
 """Точка входа в приложение
-Театр начинается с вешалки, а бот начинается с точки входа. 
-Пусть это будет файл bot.py. 
-В нём мы определим асинхронную функцию main(), 
-в которой создадим необходимые объекты и запустим поллинг. 
-Какие объекты являются необходимыми? 
-Во-первых, разумеется, бот. 
-Их может быть несколько, но об этом как-нибудь в другой раз. 
-Во-вторых, диспетчер. 
+Театр начинается с вешалки, а бот начинается с точки входа.
+Пусть это будет файл bot.py.
+В нём мы определим асинхронную функцию main(),
+в которой создадим необходимые объекты и запустим поллинг.
+Какие объекты являются необходимыми?
+Во-первых, разумеется, бот.
+Их может быть несколько, но об этом как-нибудь в другой раз.
+Во-вторых, диспетчер.
 Он занимается приёмом событий от Telegram и раскидыванием их по хэндлерам через фильтры и мидлвари."""
 
 
 # Запуск бота
 async def main():
+
     bot = Bot(token=config.bot_token.get_secret_value())
     dp = Dispatcher()
 
@@ -46,6 +48,54 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    # логгирование
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    # ---логгирование---------------
+    logging.basicConfig(level=logging.INFO,
+                        format='[{asctime}] #{levelname:8} {filename}:'
+                               '{lineno} - {name} - {message}',
+                        style='{',
+                        stream=sys.stdout,
+                        encoding='utf-8')
+
+    # создаем логгер
+    root_logger = logging.getLogger() # корневой. без имени
+    root_logger.setLevel(logging.INFO)
+
+    # Удаляем другие обработчики корневого логгера, если они есть
+    for handler in root_logger.handlers:
+        root_logger.removeHandler(handler)
+
+    # ==ХЕНДЛЕР ДЛЯ ЗАПИСИ В ФАЙЛ================================
+    # Инициализируем хэндлер, который будет записывать логи в файл
+    file_handler = logging.FileHandler(filename='logs.log',
+                                       mode='a', # 'a'-добавление , 'w'- перезапись файла
+                                       encoding='utf-8')
+
+    # создаем формат для logger формат
+    # Установка формата для файла
+    file_formatter = logging.Formatter(fmt='[{asctime}] #{levelname:8} {filename}:'
+                                           '{lineno} - {name} - {message}',
+                                       style='{')
+    #прописываем в хендлер
+    file_handler.setFormatter(fmt=file_formatter)
+    file_handler.setLevel(level=logging.WARNING)   # уровень WARNING  и выше
+    #=====================================================
+    #==ХЕНДЛЕР ДЛЯ ЗАПИСИ В КОНСОЛЬ=======================
+    # Обработчик для вывода логов в консоль
+    console_handler = logging.StreamHandler(stream=sys.stdout)
+    console_formatter = logging.Formatter(fmt='[{asctime}] #{levelname:8} {filename}:'
+                                           '{lineno} - {name} - {message}',
+                                       style='{')
+    # прописываем в хендлер
+    console_handler.setFormatter(fmt=console_formatter)
+    #=====================================================
+
+    # Добавляем хэндлеры логгеру
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(console_handler)
+
+    #-------------------------------
     asyncio.run(main())
+
+    # Закрываем файловый хендлер после завершения работы
+    file_handler.close()
+
--- test/test.py
+++ test/test.py
@@ -1,24 +1,69 @@
 import logging
+import sys
 
-logging.basicConfig(level=logging.DEBUG)
+#класс фильтрации
+class ErrorLogFilter(logging.Filter):
+    # Переопределяем метод filter, который принимает `self` и `record`
+    # Переменная рекорд будет ссылаться на объект класса LogRecord
+    def filter(self, log_record) -> bool:
 
-
-
-
-
-
-print(logging.DEBUG)
-print(logging.INFO)
-print(logging.WARNING)
-print(logging.ERROR)
-print(logging.CRITICAL)
-
-loger = logging.getLogger(__name__)
+        return log_record.levelname == 'ERROR' and 'важно' in log_record.msg.lower()
 
 
+# Определяем первый вид форматирования
+format_1 = '#%(levelname)-8s [%(asctime)s] - %(filename)s:'\
+           '%(lineno)d - %(name)s - %(message)s'
+# Определяем второй вид форматирования
+format_2 = '#{levelname:8} [{asctime}] - {filename}:'\
+           '{lineno} - {name} - {message}'
 
-loger.warning('предупреждение')
-loger.critical('Опасность', stack_info=True)
+# Инициализируем первый форматтер
+formatter_1 = logging.Formatter(fmt=format_1)
+# Инициализируем второй форматтер
+formatter_2 = logging.Formatter(fmt=format_2,
+                                style='{')
+
+# Создаем логгер
+logger = logging.getLogger(__name__)
+
+# Инициализируем хэндлер, который будет перенаправлять логи в stderr
+stderr_handler = logging.StreamHandler()
+# Инициализируем хэндлер, который будет перенаправлять логи в stdout
+stdout_handler = logging.StreamHandler(sys.stdout)
+# Инициализируем хэндлер, который будет записывать логи в файл
+file_handler = logging.FileHandler('logs.log', mode='w') # mode='w' - перезапись в файл
+
+# Устанавливаем форматтеры для хэндлеров
+stderr_handler.setFormatter(formatter_1)
+stdout_handler.setFormatter(formatter_2)
+file_handler.setFormatter(formatter_2)
+
+# Подключаем фильтр к хэндлеру
+file_handler.addFilter(ErrorLogFilter())
+
+# Добавляем хэндлеры логгеру
+logger.addHandler(stdout_handler)
+logger.addHandler(stderr_handler)
+logger.addHandler(file_handler)
+
+# Создаем лог
+logger.warning('Важно! Это лог с предупреждением!')
+logger.error('Важно! Это лог с ошибкой!')
+logger.info('Важно! Это лог с уровня INFO!')
+logger.error('Это лог с ошибкой!')
+
+logger.exception(" лог эксепт")
+
+
+
+
+# print(logging.DEBUG)
+# print(logging.INFO)
+# print(logging.WARNING)
+# print(logging.ERROR)
+# print(logging.CRITICAL)
+
+
 
 #API_URL = 'https://api.telegram.org/bot'
 #BOT_TOKEN = '7475484223:AAFGC4PkHZPkJzLFNpFMOhU3LK18_DC8weQ'
